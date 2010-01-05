b5a7afaba1079fb2d77701c41217460c288c4969
Jean-Baptiste Queru <jbq@google.com>
2010-01-05 11:09:51 -0800

    explicit set an "optional" tag

--- Android.mk
+++ Android.mk
@@ -15,6 +15,8 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_MODULE_TAGS := optional
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += \
     src/com/android/exchange/IEmailService.aidl \
