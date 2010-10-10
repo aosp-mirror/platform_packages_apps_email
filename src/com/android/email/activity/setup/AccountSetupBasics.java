43f2321d10a9ec3efd649cd257aee8d88ea87e08
Daisuke Miyakawa <dmiyakawa@google.com>
2010-10-10 15:14:24 -0700

    Modify code style.
    Change-Id: I40eeb738ca2dcc7f21ddbc603b03debde5ac27d3

--- src/com/android/email/activity/setup/AccountSetupBasics.java
+++ src/com/android/email/activity/setup/AccountSetupBasics.java
@@ -149,6 +149,21 @@ public class AccountSetupBasics extends AccountSetupActivity
         }
     }
 
+    @Override
+    public void finish() {
+        if (mAccountAuthenticatorResponse != null) {
+            // send the result bundle back if set, otherwise send an error.
+            if (mResultBundle != null) {
+                mAccountAuthenticatorResponse.onResult(mResultBundle);
+            } else {
+                mAccountAuthenticatorResponse.onError(AccountManager.ERROR_CODE_CANCELED,
+                        "canceled");
+            }
+            mAccountAuthenticatorResponse = null;
+        }
+        super.finish();
+    }
+
     /**
      * Implements AccountCheckSettingsFragment.Callbacks
      *
@@ -262,20 +277,5 @@ public class AccountSetupBasics extends AccountSetupActivity
     public void onProceedManual(boolean allowAutoDiscover) {
         SetupData.setAllowAutodiscover(allowAutoDiscover);
         AccountSetupAccountType.actionSelectAccountType(this);
-    }
-
-    @Override
-    public void finish() {
-        if (mAccountAuthenticatorResponse != null) {
-            // send the result bundle back if set, otherwise send an error.
-            if (mResultBundle != null) {
-                mAccountAuthenticatorResponse.onResult(mResultBundle);
-            } else {
-                mAccountAuthenticatorResponse.onError(AccountManager.ERROR_CODE_CANCELED,
-                        "canceled");
-            }
-            mAccountAuthenticatorResponse = null;
-        }
-        super.finish();
     }
 }
