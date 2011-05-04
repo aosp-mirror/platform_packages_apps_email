78959916e771114ff8c48fc181e34a7dff0aa672
Ben Komalo <benkomalo@google.com>
2011-05-04 10:15:35 -0700

    Add support for client side SSL certificates
    This introduces the ability for clients (i.e. the exchange service) to
    register "special connection types" that use a client certificate stored
    in the system keystore. The alias is encoded into the URI scheme for
    those clients, and the socket factory used for those connections will
    use the approprate KeyManager.
    
    Lots of TODO's, including bubbling a lot of this up to the higher level
    and wiring the UI to actually set the alias in the HostAuth table.
    
    Change-Id: If5e1901c5b58731fdabd3e6b6da7198134b512d2

--- emailcommon/src/com/android/emailcommon/utility/SSLUtils.java
+++ emailcommon/src/com/android/emailcommon/utility/SSLUtils.java
@@ -16,31 +16,60 @@
 
 package com.android.emailcommon.utility;
 
+import android.content.Context;
 import android.net.SSLCertificateSocketFactory;
+import android.security.KeyChain;
+import android.security.KeyChainException;
+import android.util.Log;
 
-import javax.net.ssl.SSLSocketFactory;
+import java.net.Socket;
+import java.security.Principal;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.X509ExtendedKeyManager;
 
 public class SSLUtils {
-    private static SSLSocketFactory sInsecureFactory;
-    private static SSLSocketFactory sSecureFactory;
+    private static SSLCertificateSocketFactory sInsecureFactory;
+    private static SSLCertificateSocketFactory sSecureFactory;
+
+    private static final boolean LOG_ENABLED = false;
+    private static final String TAG = "Email.Ssl";
 
     /**
-     * Returns a {@link SSLSocketFactory}.  Optionally bypass all SSL certificate checks.
+     * Returns a {@link javax.net.ssl.SSLSocketFactory}.
+     * Optionally bypass all SSL certificate checks.
      *
      * @param insecure if true, bypass all SSL certificate checks
      */
-    public synchronized static final SSLSocketFactory getSSLSocketFactory(boolean insecure) {
+    public synchronized static final SSLCertificateSocketFactory getSSLSocketFactory(
+            boolean insecure) {
         if (insecure) {
             if (sInsecureFactory == null) {
-                sInsecureFactory = SSLCertificateSocketFactory.getInsecure(0, null);
+                sInsecureFactory = (SSLCertificateSocketFactory)
+                        SSLCertificateSocketFactory.getInsecure(0, null);
             }
             return sInsecureFactory;
         } else {
             if (sSecureFactory == null) {
-                sSecureFactory = SSLCertificateSocketFactory.getDefault(0, null);
+                sSecureFactory = (SSLCertificateSocketFactory)
+                        SSLCertificateSocketFactory.getDefault(0, null);
             }
             return sSecureFactory;
         }
+    }
+
+    /**
+     * Returns a {@link org.apache.http.conn.ssl.SSLSocketFactory SSLSocketFactory} for use with the
+     * Apache HTTP stack.
+     */
+    public static SSLSocketFactory getHttpSocketFactory(boolean insecure) {
+        SSLCertificateSocketFactory underlying = getSSLSocketFactory(insecure);
+        // TODO: register a keymanager that will simply listen for requests for a client
+        // certificate so that higher levels know to ask the user for such credentials.
+        return new SSLSocketFactory(underlying);
     }
 
     /**
@@ -69,5 +98,111 @@ public class SSLUtils {
             }
         }
         return sb.toString();
+    }
+
+    @SuppressWarnings("unused")
+    private static abstract class StubKeyManager extends X509ExtendedKeyManager {
+        @Override public abstract String chooseClientAlias(
+                String[] keyTypes, Principal[] issuers, Socket socket);
+
+        @Override public abstract X509Certificate[] getCertificateChain(String alias);
+
+        @Override public abstract PrivateKey getPrivateKey(String alias);
+
+
+        // The following methods are unused.
+
+        @Override
+        public final String chooseServerAlias(
+                String keyType, Principal[] issuers, Socket socket) {
+            // not a client SSLSocket callback
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public final String[] getClientAliases(String keyType, Principal[] issuers) {
+            // not a client SSLSocket callback
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public final String[] getServerAliases(String keyType, Principal[] issuers) {
+            // not a client SSLSocket callback
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    /**
+     * A {@link KeyManager} that reads uses credentials stored in the system {@link KeyChain}.
+     */
+    public static class KeyChainKeyManager extends StubKeyManager {
+        private final String mClientAlias;
+        private final X509Certificate[] mCertificateChain;
+        private final PrivateKey mPrivateKey;
+
+        /**
+         * Builds an instance of a KeyChainKeyManager using the given certificate alias.
+         * If for any reason retrieval of the credentials from the system {@link KeyChain} fails,
+         * a {@code null} value will be returned.
+         */
+        public static KeyChainKeyManager fromAlias(Context context, String alias) {
+            X509Certificate[] certificateChain;
+            try {
+                certificateChain = KeyChain.getCertificateChain(context, alias);
+            } catch (KeyChainException e) {
+                Log.e(TAG, "Unable to retrieve certificate chain for [" + alias + "] due to "
+                        + e);
+                return null;
+            } catch (InterruptedException e) {
+                Log.e(TAG, "Unable to retrieve certificate chain for [" + alias + "] due to "
+                        + e);
+                return null;
+            }
+
+            PrivateKey privateKey;
+            try {
+                privateKey = KeyChain.getPrivateKey(context, alias);
+            } catch (KeyChainException e) {
+                Log.e(TAG, "Unable to retrieve private key for [" + alias + "] due to " + e);
+                return null;
+            } catch (InterruptedException e) {
+                Log.e(TAG, "Unable to retrieve private key for [" + alias + "] due to " + e);
+                return null;
+            }
+
+            return new KeyChainKeyManager(alias, certificateChain, privateKey);
+        }
+
+        private KeyChainKeyManager(
+                String clientAlias, X509Certificate[] certificateChain, PrivateKey privateKey) {
+            mClientAlias = clientAlias;
+            mCertificateChain = certificateChain;
+            mPrivateKey = privateKey;
+        }
+
+
+        @Override
+        public String chooseClientAlias(String[] keyTypes, Principal[] issuers, Socket socket) {
+            if (LOG_ENABLED) {
+                Log.i(TAG, "Requesting a client cert alias for " + Arrays.toString(keyTypes));
+            }
+            return mClientAlias;
+        }
+
+        @Override
+        public X509Certificate[] getCertificateChain(String alias) {
+            if (LOG_ENABLED) {
+                Log.i(TAG, "Requesting a client certificate chain for alias [" + alias + "]");
+            }
+            return mCertificateChain;
+        }
+
+        @Override
+        public PrivateKey getPrivateKey(String alias) {
+            if (LOG_ENABLED) {
+                Log.i(TAG, "Requesting a client private key for alias [" + alias + "]");
+            }
+            return mPrivateKey;
+        }
     }
 }
