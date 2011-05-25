c1a20e7568c3e7d7c579a0adf948d2f840d9c517
Ben Komalo <benkomalo@google.com>
2011-05-25 15:44:41 -0700

    Step 2 in 7" support for tablets.
    This is a cherry-pick of f872876699a22d90bc39f7efaac5b21e224e7c82 :
    - Use new recommended sw600dp instead of w600dp to avoid having the
    "personality" change on configuration change
    - remove the use of xlarge/large for specializing values since the
    framework team discourages any use of the buckets (and have also changed
    the ordering so that they take lower priority)
    
    Change-Id: I645bfb7cddcda8b53569af927f6fa23180e73e1f

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -55,6 +55,8 @@ $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/com.an
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/com.android.emailcommon_intermediates)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/com.android.emailcommon_intermediates)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/com.android.emailcommon_intermediates)
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Email_intermediates)
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/EmailGoogle_intermediates)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
