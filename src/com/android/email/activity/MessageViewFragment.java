7667632eb24653ef208261651298996f5fdf6911
Jorge Lugo <jlugo@google.com>
2011-07-07 20:10:54 -0700

    Improve accessibility for stars and settings
    Adding and removing a star triggers the appropriate accessibility
    In incoming/outgoing settings, added EditText contentDescriptions.
    
    Change-Id: Ibab461f1425b3ebf3579ebc1d0b36d1a9a5efdb2

--- src/com/android/email/activity/MessageViewFragment.java
+++ src/com/android/email/activity/MessageViewFragment.java
@@ -36,6 +36,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.accessibility.AccessibilityEvent;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
@@ -253,6 +254,19 @@ public class MessageViewFragment extends MessageViewFragmentBase
     }
 
     /**
+     * Sets the content description for the star icon based on whether it's currently starred.
+     */
+    private void setStarContentDescription(boolean isFavorite) {
+        if (isFavorite) {
+            mFavoriteIcon.setContentDescription(
+                    mContext.getResources().getString(R.string.remove_star_action));
+        } else {
+            mFavoriteIcon.setContentDescription(
+                    mContext.getResources().getString(R.string.set_star_action));
+        }
+    }
+
+    /**
      * Toggle favorite status and write back to provider
      */
     private void onClickFavorite() {
@@ -262,6 +276,10 @@ public class MessageViewFragment extends MessageViewFragmentBase
         // Update UI
         boolean newFavorite = ! message.mFlagFavorite;
         mFavoriteIcon.setImageDrawable(newFavorite ? mFavoriteIconOn : mFavoriteIconOff);
+
+        // Handle accessibility event
+        setStarContentDescription(newFavorite);
+        mFavoriteIcon.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_FOCUSED);
 
         // Update provider
         message.mFlagFavorite = newFavorite;
@@ -402,6 +420,10 @@ public class MessageViewFragment extends MessageViewFragmentBase
     @Override
     protected void onPostLoadBody() {
         onMarkMessageAsRead(true);
+
+        // Initialize star content description for accessibility
+        Message message = getMessage();
+        setStarContentDescription(message.mFlagFavorite);
     }
 
     @Override
