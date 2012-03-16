05b99a0d8cbf6e9eab0b12e596e9afbff4cec9d4
Vikram Aggarwal <viki@google.com>
2012-03-16 15:12:47 -0700

    AutoAdvance.LIST is safer than AutoAdvance.NEWER
    Change-Id: I72ba37cb8a7259c13afcfaabb70a11d65f536b69

--- src/com/android/email/provider/EmailProvider.java
+++ src/com/android/email/provider/EmailProvider.java
@@ -2042,7 +2042,7 @@ outer:
     private static final ProjectionMap sAccountSettingsMap = ProjectionMap.builder()
         .add(UIProvider.SettingsColumns.SIGNATURE, AccountColumns.SIGNATURE)
         .add(UIProvider.SettingsColumns.AUTO_ADVANCE,
-                Integer.toString(UIProvider.AutoAdvance.NEWER))
+                Integer.toString(UIProvider.AutoAdvance.LIST))
         .add(UIProvider.SettingsColumns.MESSAGE_TEXT_SIZE,
                 Integer.toString(UIProvider.MessageTextSize.NORMAL))
         .add(UIProvider.SettingsColumns.SNAP_HEADERS,
