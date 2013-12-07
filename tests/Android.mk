475a7626fb094723c767682a4739a5de34d0a3ad
Jerry Xie <jjx@google.com>
2013-12-06 17:33:36 -0800

    Add makefile for EmailTests
    Change-Id: Ic441b5cea9efd0b714a00a69c5b8483ddd11fafa
    (cherry picked from commit d241290f0b09df8cca0b1472a48b6b26895eae9d)

---
+++ tests/Android.mk
@@ -0,0 +1,40 @@
+# Copyright 2011, The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH:= $(call my-dir)
+
+src_dirs := src
+res_dirs := res
+
+##################################################
+
+include $(CLEAR_VARS)
+
+# We only want this apk build for tests.
+LOCAL_MODULE_TAGS := tests
+
+# LOCAL_JAVA_LIBRARIES := android.test.runner
+
+LOCAL_SDK_VERSION := 14
+LOCAL_PACKAGE_NAME := EmailTests
+LOCAL_INSTRUMENTATION_FOR := Email
+
+LOCAL_SRC_FILES := $(call all-java-files-under, $(src_dirs))
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+
+include $(BUILD_PACKAGE)
+
+# Build all sub-directories
+include $(call all-makefiles-under,$(LOCAL_PATH))
