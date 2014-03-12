40baa96c3f5c2a2f50ea243f83bc62c8c65a69b9
Martin Hibdon <mhibdon@google.com>
2014-03-12 11:00:24 -0700

    Add the password failed warning text
    Now, if you fail to authenticate on the credentials fragment,
    instead of taking you to the full accountSetupIncoming,
    it just takes you back to the credentials fragment with
    a warning that your password is wrong.
    Also, make it so that pressing "next" on the password IME behaves
    the same as pressing the Next button.
    
    Change-Id: Ice91c842659c33ba6f8ac876356a79265c703e2e

--- src/com/android/email/activity/setup/AccountCredentials.java
+++ src/com/android/email/activity/setup/AccountCredentials.java
@@ -36,7 +36,7 @@ public class AccountCredentials extends Activity
         final String protocol = getIntent().getStringExtra(EXTRA_PROTOCOL);
 
         final AccountSetupCredentialsFragment f =
-                AccountSetupCredentialsFragment.newInstance(emailAddress, protocol);
+                AccountSetupCredentialsFragment.newInstance(emailAddress, protocol, false);
         getFragmentManager().beginTransaction()
                 .add(R.id.account_credentials_fragment_container, f, CREDENTIALS_FRAGMENT_TAG)
                 .commit();
